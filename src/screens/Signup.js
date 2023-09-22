7f02f45cfa59e712068c77d6e7ffd63605f3d199
Bhomesh <bhomeshrazdan@gmail.com>
2023-09-22 09:32:21 +0530

    bug fixed

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -2,8 +2,6 @@ import React from "react";
 // import router from "../../backend/Routes/CreateUser";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import Card from "../components/Card";
-import Carousel from "../components/Carousel";
 
 export default function Signup() {
   return (
@@ -13,40 +11,40 @@ export default function Signup() {
       </div>
       <div>
         <form>
-          <div class="form-group">
+          <div className="form-group">
             <label for="exampleInputEmail1">Email address</label>
             <input
               type="email"
-              class="form-control"
-              s
+              className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Enter email"
+              
             />
-            <small id="emailHelp" class="form-text text-muted">
+            <small id="emailHelp" className="form-text text-muted">
               We'll never share your email with anyone else.
             </small>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label for="exampleInputPassword1">Password</label>
             <input
               type="password"
-              class="form-control"
+              className="form-control"
               id="exampleInputPassword1"
               placeholder="Password"
             />
           </div>
-          <div class="form-check">
+          <div className="form-check">
             <input
               type="checkbox"
-              class="form-check-input"
+              className="form-check-input"
               id="exampleCheck1"
             />
-            <label class="form-check-label" for="exampleCheck1">
+            <label className="form-check-label" for="exampleCheck1">
               Check me out
             </label>
           </div>
-          <button type="submit" class="btn btn-primary">
+          <button type="submit" className="btn btn-primary">
             Submit
           </button>
         </form>
